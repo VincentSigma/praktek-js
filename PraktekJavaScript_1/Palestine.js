497399a3adb260041328d0ed0b3d9daadcbd3b6e
VincentSigma <icenhebat@gmail.com>
2024-01-15 10:35:27 +0700

    Initial Commit

---
+++ PraktekJavaScript_1/Palestine.js
@@ -0,0 +1,18 @@
+//Info negara
+const namanegara = 'Palestine';
+const benua = 'Asia';
+let populasi = 4.92300;
+const kepulauan  = false;
+const bahasa = 'Arab';
+
+console.log('NamaNegara:', namanegara);
+console.log('Benua: ', benua);
+console.log('Populasi: ', populasi);
+console.log('Bahasa: ', bahasa);
+
+
+
+
+
+
+
