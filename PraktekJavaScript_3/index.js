90bb02395e4cd87a30e88fe7a32ad61c42a32542
VincentSigma <icenhebat@gmail.com>
2024-01-16 14:27:51 +0700

    tambah praktek 3

---
+++ PraktekJavaScript_3/index.js
@@ -0,0 +1,15 @@
+//Info negara
+const namanegara = 'Palestine';
+const benua = 'Asia barat';
+let populasi = 4.92300;
+const kepulauan  = false;
+const bahasa = 'Arab';
+
+console.log('NamaNegara:', namanegara);
+console.log('Benua: ', benua);
+console.log('Populasi: ', populasi);
+console.log('Bahasa: ', bahasa);
+
+//Template literal
+console.log(`Negara ${namanegara} adalah negara yang berada di benua ${benua}, dengan populasi sebanyak ${populasi} jiwa, bahasa nasional bahasa ${bahasa}.`);
+
